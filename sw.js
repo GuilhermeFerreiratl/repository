ffbaf360b29533c70c7ef076d5cec6e5553fefbb
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-08-01 09:06:33 -0300

    Upgrade service worker to v2 with cache handling
    Updated service worker to version 2 with cache management improvements.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-cassino-v1';
+const CACHE_NAME = 'pwa-cassino-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -8,11 +8,27 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
